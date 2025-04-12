22dbffd49fb552bc9e72b48e9ef8fb58a3ee8a41
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-04-12 18:09:03 +0200

    Some more order brought in to the init processes and additional logging.

--- modules/suspects/project/TD_uv/extTDuv.py
+++ modules/suspects/project/TD_uv/extTDuv.py
@@ -12,8 +12,8 @@ import zipfile
 from types import ModuleType
 from typing import List
 from pathlib import Path
-
 import importlib
+# import importlib
 import sys
 from copy import copy
 
@@ -21,16 +21,34 @@ import platform
 
 def UvRequired(function):
 	def wrapper(self, *args, **kwargs):
-		if not self.checkUv: self.SetupUv()
+		self.Log("UV Required. Checking.")
+		if not self.checkUv: 
+			self.Log("UV Not found. Running setup.")
+			self.SetupUv()
 		return function(self, *args, **kwargs)
+	wrapper.__name__ = f"UvRequired_{function.__name__}"
 	return wrapper
 
 def ProjectRequired(function):
 	def wrapper(self, *args, **kwargs):
-		if not self.checkProject: self.SetupProject()
+		self.Log("Project Required. Checking.")
+		if not self.checkProject: 
+			self.Log("Project not found. Running setup.")
+			self.SetupProject()
 		return function(self, *args, **kwargs)
+	wrapper.__name__ = f"ProjectRequired_{function.__name__}"
 	return wrapper
  
+
+def VenvRequired(function):
+	def wrapper(self, *args, **kwargs):
+		self.Log("Venv Required. Checking.")
+		if not self.checkVenv: 
+			self.Log("Venv. Running setup.")
+			self.SetupVenv()
+		return function(self, *args, **kwargs)
+	wrapper.__name__ = f"VenvRequired_{function.__name__}"
+	return wrapper
 
 class extTDuv:
 	"""
@@ -40,19 +58,21 @@ class extTDuv:
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
 		self.Log = self.ownerComp.op("logger").Log
-
+		self.ownerComp.op("logger").par.Clear.pulse()
 		class Mount(object):
 			def __init__(mountSelf, clearModules = False):
 				mountSelf.clearModules = clearModules
 				pass
 
 			def __enter__(mountSelf):
+				self.Log("Entering ENV Mount")
 				if mountSelf.clearModules:
 					mountSelf.modules = sys.modules.copy()
 					sys.modules = {}
 				self.mountEnv()
 
 			def __exit__(mountSelf, type, value, traceback):
+				self.Log("Exiting ENV Mount.")
 				self.unmountEnv()
 				if mountSelf.clearModules:
 					sys.modules = mountSelf.modules	
@@ -71,9 +91,11 @@ class extTDuv:
 				pass
 
 			def __enter__(mountSelf):
+				self.Log("Entering Module Mount", mountSelf.moduleName)
 				if mountSelf.clearModules:
 					mountSelf.modules = sys.modules.copy()
 					sys.modules = {}
+				# Now we can be sure that it exists. I assume importlib is checking if the path is valid already when setting the value?
 				self.mountEnv()
 				try:
 					return self.ImportModule( 
@@ -86,7 +108,7 @@ class extTDuv:
 					raise e
 
 			def __exit__(mountSelf, exc_type, exc_val, exc_tb):
-				debug("Exiting Mound Module")
+				self.Log("Exiting Module Mount")
 				self.unmountEnv()
 				if mountSelf.clearModules:
 					sys.modules = mountSelf.modules	
@@ -105,15 +127,24 @@ class extTDuv:
 	
 		raise NotImplemented(f"{sys.platform} OS Not Supported.")
 	
-	@property
+	#@property
+	@cached_property
 	def checkUv(self):
 		result = self.runUvCommand(["version", "-q"])
+		self.Log("Checking UV", result == 0)
 		return result == 0
 	
-	@property
+	# @property
+	@cached_property
 	def checkProject(self):
+		self.Log("Checking Project", Path("pyproject.toml").is_file())
 		return Path("pyproject.toml").is_file()
-		# return Path(self.localLibPath).is_dir()
+	
+	# @property
+	@cached_property
+	def checkVenv(self):
+		self.Log("Checking Project", Path(self.localLibPath).is_dir())
+		return Path(self.localLibPath).is_dir()
 	
 	@property
 	def uvFolder(self):
@@ -125,18 +156,20 @@ class extTDuv:
 		return ".venv/Lib/site-packages"
 
 	def mountEnv(self):
+		self.Log("Mounting ENV.", self.localLibPath)
 		self._pathCopy = copy( sys.path )
 		self._envCopy = copy( os.environ )
 		sys.path.insert(0, self.localLibPath)
 		os.environ['PYTHONPATH'] = self.localLibPath
-
+		
 	def unmountEnv(self):
+		self.Log("UnMounting ENV.")
 		sys.path = self._pathCopy
 		os.environ = self._envCopy
 
 	def runUvCommand(self, commands):
 		if isinstance( commands, str): commands = commands.split(" ")
-		
+		self.Log("Running ENV Command", " ".join( commands ))
 		return subprocess.call( ["uv"] + commands, env= {
 			**os.environ,
 			"PATH" : ";".join([ self.uvFolder ] + os.environ["PATH"].split(";"))
@@ -144,6 +177,7 @@ class extTDuv:
 		 shell=True )
 
 	def SetupUv(self):
+		self.Log("Setting up UV.")
 		downloadUvZip = self.ownerComp.op("uvDependency").GetRemoteFilepath()
 		if Path( downloadUvZip).suffix == "gz":
 			raise NotImplemented("gz files not implemented. Its like Matroschka and I am to lazy rn to take care of that...")
@@ -151,16 +185,24 @@ class extTDuv:
 		with zipfile.ZipFile(downloadUvZip, 'r') as zip_ref:
 			zip_ref.extractall(self.uvFolder)
 		
+		del self.checkUv
+
 	@UvRequired
 	def SetupProject(self):
 		result = self.runUvCommand(f"init --python {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}") == 0
-		self.runUvCommand("add pip-install-test")
-		self.runUvCommand("remove pip-install-test")
-		importlib.invalidate_caches()
+		del self.checkProject
+		return result
+	
+	@ProjectRequired
+	@UvRequired
+	def SetupVenv(self):
+		result = self.runUvCommand(f"venv") == 0
+		del self.checkVenv
 		return result
 
 	@UvRequired
 	@ProjectRequired
+	@VenvRequired
 	def InstallPackage(self, packagePipName:str, additional_settings:List[str] = []):
 		sourceIndex = ""
 		for _index, element in enumerate( additional_settings ):
@@ -168,16 +210,17 @@ class extTDuv:
 				sourceIndex = additional_settings[ _index +1 ]
 				break
 		self.ownerComp.op("dependecyTableRepo").Repo.row( packagePipName ) or self.ownerComp.op("dependecyTableRepo").Repo.appendRow([packagePipName, sourceIndex])
+		self.Log("Installing Package", packagePipName)
 		return self.runUvCommand(["add", packagePipName] + additional_settings) == 0
 
-
-	@ProjectRequired
+	@VenvRequired
 	def TestModule(self, module:str, silent:bool = False):
 		"""
 			Check if a given moduel is alread installed. 
 			Note, this does not test for a package but the module itself.
 		"""
 		self.Log("Testing for package", module)
+		
 		with self.Mount():
 			try:
 				foundModule:ModuleType = importlib.util.find_spec( module )	
@@ -191,9 +234,9 @@ class extTDuv:
 				
 			if not silent: ui.messageBox('Does exist', 'The package is installed')
 			return True
-	
-	@UvRequired
+
 	@ProjectRequired
+	@VenvRequired
 	def ImportModule(self, moduleName:str, pipPackageName:str = '', additionalSettings:List[str]=[] ):
 		"""
 			Installs the module if not already installed and returns the module.
@@ -202,7 +245,9 @@ class extTDuv:
 		"""
 
 		_pipPackageName = pipPackageName or moduleName
+		self.Log("Importing Module", moduleName, pipPackageName)
 		if not self.TestModule(moduleName, silent = True): 
+			self.Log("Not Found, running install.")
 			if not self.InstallPackage(_pipPackageName, additional_settings=additionalSettings):
 				raise ModuleNotFoundError
 		return importlib.import_module(moduleName)
